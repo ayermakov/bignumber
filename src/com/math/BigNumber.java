e480cf0adcdfad425f9abfd22c0e5848ed42c575
Alexey <alexermakov@ukr.net>
2022-04-29 19:06:13 +0300

    Implemented isGreaterThan. Refactoring. Completed add function.

--- src/com/math/BigNumber.java
+++ src/com/math/BigNumber.java
@@ -2,48 +2,50 @@ package com.math;
 
 import java.util.Arrays;
 
-public class BigInt {
-    // bigInt stores decimal values in ASC order (value 123 is stored as {3,2,1}).
-    private short[] value;
-    private StringBuffer lastResult = new StringBuffer();
-    private int indexOfNextPart;
-    private int indexOfHasNextDigit = 0;
+public class BigNumber {
+    // BigInt stores decimal values in reverse order (value 123 is stored as {3,2,1}).
+    private final short[] value;
     private boolean isNegative;
 
-    public BigInt(String bigIntInString, boolean negative) {
+    public BigNumber(String bigIntInString, boolean negative) {
         value = new short[bigIntInString.length()];
         for(int i = value.length - 1, j = 0; i >= 0 && j < bigIntInString.length(); i--, j++) {
             value[i] = Short.parseShort(String.valueOf(bigIntInString.charAt(j)));
         }
 
-        indexOfNextPart = value.length - 1;
         isNegative = negative;
     }
 
-    private BigInt(short[] newValue) {
+    private BigNumber(short[] newValue) {
         value = newValue;
     }
 
-    public BigInt add(BigInt addedValue) {
+    public BigNumber add(BigNumber addedValue) {
         short[] value2 = addedValue.getValue();
 
         if(isNegative) {
             if (addedValue.isNegative()) {
-                if (value.length >= value2.length)
+                if (this.isGreaterThan(value2))
                     return calculateAdd(value, value2); //with neg flag
                 else
                     return calculateAdd(value2, value); //with neg flag
             } else {
-                if (value.length >= value2.length)
-                    return calculateDistract(value, value2);
+                if (this.isGreaterThan(value2))
+                    return calculateDistract(value, value2);//with neg flag
                 else
                     return calculateDistract(value2, value);//with plus flag
             }
         } else {
             if (addedValue.isNegative()) {
-                return this;
+                if (this.isGreaterThan(value2))
+                    return calculateDistract(value, value2); //with plus flag
+                else
+                    return calculateDistract(value2, value); //with neg flag
             } else {
-                return calculateAdd(value, value2);//no flag
+                if (this.isGreaterThan(value2))
+                    return calculateAdd(value, value2);//with plus flag
+                else
+                    return calculateAdd(value2, value);//with plus flag
             }
         }
     }
@@ -52,16 +54,16 @@ public class BigInt {
      * This method add two big numbers.
      * First argument must be a larger array than the second one.
      *
-     * @param value1
-     * @param value2
-     * @return
+     * @param value1 is the biggest one
+     * @param value2 is the less one
+     * @return new BigNumber
      */
-    private BigInt calculateAdd(short[] value1, short[] value2) {
+    private BigNumber calculateAdd(short[] value1, short[] value2) {
         int j = 0;
         boolean pernose = false;
         short[] result = new short[value1.length];
         for (int i = 0; i < value1.length; i++) {
-            int sum = 0;
+            int sum;
 
             if(j >= value2.length)
                 sum = value1[i];
@@ -85,29 +87,22 @@ public class BigInt {
             result[result.length - 1] = 1;
         }
 
-        return new BigInt(result);
+        return new BigNumber(result);
     }
 
-    public BigInt distract(BigInt addedValue) {
+    public BigNumber distract(BigNumber addedValue) {
         return this;
     }
 
-    private BigInt calculateDistract(short[] value1, short[] value2) {
+    private BigNumber calculateDistract(short[] value1, short[] value2) {
         return this;
     }
 
-    public BigInt multiply(BigInt addedValue) {
+    public BigNumber multiply(BigNumber addedValue) {
         return this;
     }
 
-    public BigInt divide(BigInt addedValue) {
-        boolean completed = false;
-        lastResult = new StringBuffer();
-        while(!completed) {
-
-            completed = true;
-        }
-
+    public BigNumber divide(BigNumber addedValue) {
         return this;
     }
 
@@ -115,26 +110,34 @@ public class BigInt {
         return value;
     }
 
-    public boolean hasNext() {
-        if(indexOfHasNextDigit < value.length)
-            return true;
-        else
-            return false;
-    }
-
     public boolean isNegative() {
         return isNegative;
     }
 
-    public boolean isGreaterThan(BigInt anotherNumber) {
+    public boolean isGreaterThan(BigNumber anotherNumber) {
         var anotherValue = anotherNumber.getValue();
+        return isGreaterThan(anotherValue);
+    }
+
+    public boolean isGreaterThan(short[] anotherValue) {
+        if(value.length > anotherValue.length)
+            return true;
+        else if(anotherValue.length > value.length)
+            return false;
+
+        for(int i = value.length - 1; i >= 0; i--) {
+            if(value[i] > anotherValue[i])
+                return true;
+            else if(anotherValue[i] > value[i])
+                return false;
+        }
 
         return false;
     }
 
     @Override
     public String toString() {
-        StringBuffer result = new StringBuffer();
+        StringBuilder result = new StringBuilder();
         for(int i = value.length - 1; i >= 0; i--) {
             result.append(value[i]);
         }
@@ -152,7 +155,7 @@ public class BigInt {
             return false;
         }
 
-        BigInt bigInt = (BigInt) o;
+        BigNumber bigInt = (BigNumber) o;
 
         if(isNegative != bigInt.isNegative)
             return false;
--- src/com/math/Main.java
+++ src/com/math/Main.java
@@ -3,13 +3,13 @@ import static org.assertj.core.api.Assertions.*;
 
 public class Main {
     public static void main(String[] args) {
-        assertThat(new BigInt("5348", false).add(new BigInt("99", false)))
-                .isEqualTo(new BigInt("5447", false));
+        assertThat(new BigNumber("5348", false).add(new BigNumber("99", false)))
+                .isEqualTo(new BigNumber("5447", false));
 
-        assertThat(new BigInt("1223", false).add(new BigInt("2138778347287428", false)))
-                .isEqualTo(new BigInt("2138778347288651", false));
+        assertThat(new BigNumber("1223", false).add(new BigNumber("2138778347287428", false)))
+                .isEqualTo(new BigNumber("2138778347288651", false));
 
-        assertThat(new BigInt("9999", false).add(new BigInt("9999", false)))
-                .isEqualTo(new BigInt("19998", false));
+        assertThat(new BigNumber("9999", false).add(new BigNumber("9999", false)))
+                .isEqualTo(new BigNumber("19998", false));
     }
 }
