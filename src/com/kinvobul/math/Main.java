5a09c0b6186d6e6db86d55e51214ccd52550f152
Alexey <alexermakov@ukr.net>
2022-05-06 18:48:08 +0300

    Added new white box tests.

--- src/com/kinvobul/math/Main.java
+++ src/com/kinvobul/math/Main.java
@@ -27,5 +27,136 @@ public class Main {
 
         assertThat(new BigNumber("9998").subtract(new BigNumber("9999")))
                 .isEqualTo(new BigNumber("-1"));
+
+        assertThat(new BigNumber("99").subtract(new BigNumber("98")))
+                .isEqualTo(new BigNumber("1"));
+
+        assertThat(new BigNumber("+9999").subtract(new BigNumber("-1")))
+                .isEqualTo(new BigNumber("10000"));
+
+        assertThat(new BigNumber("999").subtract(new BigNumber("+8")))
+                .isEqualTo(new BigNumber("991"));
+
+        // two negative
+        assertThat(new BigNumber("-9").add(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("-17"));
+
+        assertThat(new BigNumber("-8").add(new BigNumber("-9")))
+                .isEqualTo(new BigNumber("-17"));
+
+        assertThat(new BigNumber("-8").add(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("-16"));
+
+        assertThat(new BigNumber("-92").add(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("-100"));
+
+        assertThat(new BigNumber("-9").add(new BigNumber("-81")))
+                .isEqualTo(new BigNumber("-90"));
+
+        // first negative second positive
+        assertThat(new BigNumber("-9").add(new BigNumber("8")))
+                .isEqualTo(new BigNumber("-1"));
+
+        assertThat(new BigNumber("-8").add(new BigNumber("9")))
+                .isEqualTo(new BigNumber("1"));
+
+        assertThat(new BigNumber("-8").add(new BigNumber("8")))
+                .isEqualTo(new BigNumber("0"));
+
+        assertThat(new BigNumber("-92").add(new BigNumber("8")))
+                .isEqualTo(new BigNumber("-84"));
+
+        assertThat(new BigNumber("-9").add(new BigNumber("81")))
+                .isEqualTo(new BigNumber("72"));
+
+        //first positive second negative
+        assertThat(new BigNumber("9").add(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("1"));
+
+        assertThat(new BigNumber("8").add(new BigNumber("-9")))
+                .isEqualTo(new BigNumber("-1"));
+
+        assertThat(new BigNumber("8").add(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("0"));
+
+        assertThat(new BigNumber("92").add(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("84"));
+
+        assertThat(new BigNumber("9").add(new BigNumber("-81")))
+                .isEqualTo(new BigNumber("-72"));
+
+        //two positive
+        assertThat(new BigNumber("9").add(new BigNumber("8")))
+                .isEqualTo(new BigNumber("17"));
+
+        assertThat(new BigNumber("8").add(new BigNumber("9")))
+                .isEqualTo(new BigNumber("17"));
+
+        assertThat(new BigNumber("92").add(new BigNumber("8")))
+                .isEqualTo(new BigNumber("100"));
+
+        assertThat(new BigNumber("9").add(new BigNumber("81")))
+                .isEqualTo(new BigNumber("90"));
+
+        // two negative
+        assertThat(new BigNumber("-9").subtract(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("-1"));
+
+        assertThat(new BigNumber("-8").subtract(new BigNumber("-9")))
+                .isEqualTo(new BigNumber("1"));
+
+        assertThat(new BigNumber("-8").subtract(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("0"));
+
+        assertThat(new BigNumber("-92").subtract(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("-84"));
+
+        assertThat(new BigNumber("-9").subtract(new BigNumber("-81")))
+                .isEqualTo(new BigNumber("72"));
+
+        // first negative second positive
+        assertThat(new BigNumber("-9").subtract(new BigNumber("8")))
+                .isEqualTo(new BigNumber("-17"));
+
+        assertThat(new BigNumber("-8").subtract(new BigNumber("9")))
+                .isEqualTo(new BigNumber("-17"));
+
+        assertThat(new BigNumber("-8").subtract(new BigNumber("8")))
+                .isEqualTo(new BigNumber("-16"));
+
+        assertThat(new BigNumber("-92").subtract(new BigNumber("8")))
+                .isEqualTo(new BigNumber("-100"));
+
+        assertThat(new BigNumber("-9").subtract(new BigNumber("81")))
+                .isEqualTo(new BigNumber("-90"));
+
+        //first positive second negative
+        assertThat(new BigNumber("9").subtract(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("17"));
+
+        assertThat(new BigNumber("8").subtract(new BigNumber("-9")))
+                .isEqualTo(new BigNumber("17"));
+
+        assertThat(new BigNumber("8").subtract(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("16"));
+
+        assertThat(new BigNumber("92").subtract(new BigNumber("-8")))
+                .isEqualTo(new BigNumber("100"));
+
+        assertThat(new BigNumber("9").subtract(new BigNumber("-81")))
+                .isEqualTo(new BigNumber("90"));
+
+        //two positive
+        assertThat(new BigNumber("9").subtract(new BigNumber("8")))
+                .isEqualTo(new BigNumber("1"));
+
+        assertThat(new BigNumber("8").subtract(new BigNumber("9")))
+                .isEqualTo(new BigNumber("-1"));
+
+        assertThat(new BigNumber("92").subtract(new BigNumber("8")))
+                .isEqualTo(new BigNumber("84"));
+
+        assertThat(new BigNumber("9").subtract(new BigNumber("81")))
+                .isEqualTo(new BigNumber("-72"));
     }
 }
